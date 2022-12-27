749c7d65aba8963dbb6f7d8280dc7a59b818a758
shahx95 <24467345+shahx95@users.noreply.github.com>
2022-12-28 01:59:24 +0500

    Add job section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,128 @@
 import './App.css';
-import Header from "./components/Header.js"
+import React from "react"
+import Header from "./components/Header"
 import Search from "./components/Search-Bar"
+import Job from "./components/Job"
+import dummyData from "./dummyData"
+
 
 function App() {
+  let skillsArr = ["Java","React","Python","AWS","C++"]
+  
+  let [showDialogPost, setShowDialogPost] = React.useState(false)
+  
+  let [showDialogCheck, setShowDialogCheck] = React.useState({})
+  
+  let [loading, setLoading] = React.useState(true)
+  
+  let [searchForm, setSearchForm] = React.useState({
+    type: "",
+    location: ""
+  })
+  
+  let [formData, setFormData] = React.useState({
+    jobTitle: "",
+    type: "",
+    location: "",
+    companyName: "",
+    companyUrl: "",
+    jobLink: "",
+    jobDescription: "",
+    jobSkills: ""
+  })
+  
+  let [jobData, setJobData] = React.useState(dummyData)
+  
+  let [users, setUsers] = React.useState([])
+  
+  // const usersCollectionRef = collection()
+  // const getUsers = async () => {
+    
+  // }
+  
+  // React.useEffect(()=>{
+    
+  //   getUsers()
+  // },[])
+  
+  
+  function handleChange(event){
+    const {name, value, type, checked} = event.target
+    setFormData(prevFormData => ({
+      ...prevFormData, [name]: value
+    }))
+  }
+  
+ let arr = []
+  function handleJobSubmit(event){
+    event.preventDefault()
+    arr.push(formData)
+    
+    //validate form inputs
+    //add to database
+    setFormData({
+    jobTitle: "",
+    type: "",
+    location: "",
+    companyName: "",
+    companyUrl: "",
+    jobLink: "",
+    jobDescription: "",
+    jobSkills: ""
+  })
+  }
+  
+  console.log(arr[0])
+  
+   console.log(showDialogCheck)
+   
+   
+   function clearJobDialogBox(){
+     setShowDialogPost(false)
+     setFormData({
+        jobTitle: "",
+        type: "",
+        location: "",
+        companyName: "",
+        companyUrl: "",
+        jobLink: "",
+        jobDescription: "",
+        jobSkills: ""
+     })
+   }
+   
+   function submitSearch(searchForm){
+     console.log(searchForm)
+     setLoading(true)
+     let filter = dummyData.filter(obj => obj.type===searchForm.type || obj.location===searchForm.location)
+      
+          console.log(filter)
+          setLoading(false)
+     setJobData(filter)
+   }
+   
+    
+  
+   function handleSearchChange(event){
+     const {name, value, type, checked} = event.target
+    setSearchForm(prevData => ({
+      ...prevData, [name]: value
+    }))
+   }
+   
   return (
     <div className="App">
       <Header />
       <Search />
+      {console.log(dummyData)}
+
+
+
+      {dummyData.map(jobObject => {
+      
+      return <Job key={jobObject.id} id={jobObject.id} title={jobObject.title} company={jobObject.companyName} skills={jobObject.skills} time="Today" location={jobObject.location} jobType={jobObject.type}/>
+      })}
+
     </div>
   );
 }
---
+++ src/components/Job.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function({id, title,company,skills,time,location,jobType}){
+     
+    return (
+        <div className="job-container">
+            <div className="job-primary-container">
+                <h2 className="job-title">{title}</h2>
+                <a href="#" className="job-company">{company}</a>
+            </div>
+            
+            <div className="skills-container">
+                {skills.map(skill => <span className="job-skill">{skill}</span>)}
+                
+                
+            </div>
+            <div className="job-right-section">
+                <h5 className="job-attributes">{time} | {location} | {jobType}</h5>
+                <a className="job-button">Check</a>
+            </div>
+              
+             
+        </div>
+    )
+}
